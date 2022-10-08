7430134912f527e916104fd68b6c594b24415dd6
statecLata <49246584+stateclata@users.noreply.github.com>
2022-10-08 12:44:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,6 @@ def app():
     df['age']= df['age'].astype(int)
 
     st.subheader("Data diambil dari kaggle :")
-    st.markdown("[DOWNLOAD](https://www.kaggle.com/andrewmvd/heart-failure-clinical-data)")
-    st.dataframe(df)
 
     shwdata = st.multiselect('Pilih Kolom yang mau ditampilkan:', df.columns, default=[])
     st.write(df[shwdata])
